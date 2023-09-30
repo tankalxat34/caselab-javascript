64b392906ff6148af67ce5b5c88a781a40cc4243
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-09-30 11:31:53 +0300

    Теперь заменяются все подстроки

--- 1 - 27.09 (ср) - 06.10 (пт) Кейс Цензор/index.js
+++ 1 - 27.09 (ср) - 06.10 (пт) Кейс Цензор/index.js
@@ -8,13 +8,13 @@ function censor() {
 
     return function (arg1, arg2 = "") {
         if (!arg2) {
-            let replacedString = arg1;
-            for (const pare of pares) {
-                replacedString = replacedString.replace(...pare);
-            }
-            return replacedString;
+            let result = arg1;
+            pares.forEach((v, k) => {
+                result = result.replaceAll(k, v);
+            });
+            return result;
         } else {
-            pares.set(arg1, arg2);
+            pares.set(new RegExp(`${arg1}`, "gm"), arg2);
         }
     }
 }
@@ -28,3 +28,5 @@ changeScene("backend", "frontend");
 console.log(changeScene('PHP is the most popular programming language for backend web-development'));
 // JS is the most popular programming language for frontend web-development
 
+console.log(changeScene("PHP blabla PHP bla JS backend will be frontend-backend. This Backend will not be replace"));
+// JS blabla JS bla JS frontend will be frontend-frontend. This Backend will not be replace
