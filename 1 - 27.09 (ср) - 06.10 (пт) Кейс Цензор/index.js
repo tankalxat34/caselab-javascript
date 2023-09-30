5c26afe0ad2990046aac6bea326d948e502c187c
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-09-30 10:54:48 +0300

    Сделал вариант без регулярных выражений

--- 1 - 27.09 (ср) - 06.10 (пт) Кейс Цензор/index.js
+++ 1 - 27.09 (ср) - 06.10 (пт) Кейс Цензор/index.js
@@ -1,1 +1,30 @@
-// здесь будет код
+/*
+    Подстречный Александр
+    ДЗ #1 "Цензор"
+*/
+
+function censor() {
+    let pares = new Map();
+
+    return function (arg1, arg2 = "") {
+        if (!arg2) {
+            let replacedString = arg1;
+            for (const pare of pares) {
+                replacedString = replacedString.replace(...pare);
+            }
+            return replacedString;
+        } else {
+            pares.set(arg1, arg2);
+        }
+    }
+}
+
+
+
+const changeScene = censor();
+changeScene("PHP", "JS");
+changeScene("backend", "frontend");
+
+console.log(changeScene('PHP is the most popular programming language for backend web-development'));
+// JS is the most popular programming language for frontend web-development
+
