d83831a554bd21b68beabb70a09a70a404cb2d72
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-10-04 20:24:55 +0300

    Сохранил второе задание

---
+++ 2 - «02.10 (пн) - 11.10 (ср) Кейс Витрина»/app.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        id: 1,
+        title: 'Lenovo Yoga',
+        price: 3000,
+    },
+    {
+        id: 2,
+        title: 'Acer Aspire',
+        price: 1800,
+    },
+    {
+        id: 3,
+        title: 'Dell Vostro',
+        price: 3400
+    },
+];
+
+let order = [];
+
+function addToBasket(productId) {
+    // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
+
+    // TODO: если товар еще не в корзине, добавить его из массива products
+
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+function removeFromBasket(productId) {
+    // TODO: описать логику удаления товара из корзины
+
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+
+function rerenderTotalPrice() {
+    // TODO: опишите функционал подсчета общей стоимости заказа
+
+
+    // Не меняйте эту строчку
+    document.getElementById('total').innerText = totalPrice;
+}
+
+// Этот метод остается без изменений
+function renderCart() {
+    const cart = document.getElementById('basket-items');
+
+    cart.innerHTML = '';
+    order.forEach(item => {
+        const el = document.createElement('li');
+        el.innerText = item.title;
+        el.onclick = () => removeFromBasket(item.id);
+        cart.appendChild(el);
+    })
+}
